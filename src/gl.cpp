c0c2001ed7e47b2529621ea8b94325465d1ca559
finikokasulanovenda <13515029@std.stei.itb.ac.id>
2018-04-01 07:12:05 +0700

    Add 3D car

--- src/gl.cpp
+++ src/gl.cpp
@@ -26,29 +26,13 @@ void render (void) {
     unsigned int tick = SDL_GetTicks();
 
     glLoadIdentity();
-    //glRotatef(tick % 3600 * 0.1f, 0.0, 0.0, 1.0);
     
-    sky(tick % 3600 * 0.0001f, tick % 3600 * 0.0001f, tick % 3600 * 0.0001f);
-    road();
-    houses(tick % 3600 * 0.0001f);
+    //sky(tick % 3600 * 0.0001f, tick % 3600 * 0.0001f, tick % 3600 * 0.0001f);
+    //road();
+    //houses(tick % 3600 * 0.0001f);
     car();
     tires(tick % 3600 * 0.1f);
-    /*glBegin(GL_TRIANGLES);
-    glColor3f ( 1.0,1.0,0.0);
-    glVertex3f(-0.2,0.2, -3.0);
-    glColor3f ( 1.0,0.0,0.0);
-    glVertex3f(-0.2, -0.2, -3.0);
-    glColor3f ( 0.0,1.0,0.0);
-    glVertex3f( 0.2,0.2, -3.0);
-
-    glColor3f ( 1.0,1.0,1.0);
-    glVertex3f( -0.4,-0.6, -3.0);
-    glColor3f ( 1.0,1.0,1.0);
-    glVertex3f( -0.4, -0.4, -3.0);
-    glColor3f ( 1.0,1.0,1.0);
-    glVertex3f( -0.6,-0.6, -3.0);
-    glEnd();*/
-    
+/*
     glColor3f(0.3f,0.3f,0.3f);
     
     glBegin(GL_POLYGON);
@@ -56,7 +40,7 @@ void render (void) {
       glVertex3f(1.0f,-1.0f,-3.0);
       glVertex3f(1.0f,-0.5f,-3.0);
       glVertex3f(-1.0f,-0.5f,-3.0);
-    glEnd();
+    glEnd();*/
 }
 
 void houses(float movement) {
@@ -129,7 +113,8 @@ void car(void) {
     glPushMatrix();
         glBegin(GL_POLYGON);
             for (int i=0; i<8; i++) {
-                glVertex3f(vertices[i*3]/1000,vertices[(i*3)+1]/1000,vertices[(i*3)+2]/1000);
+                glVertex3f(vertices[i*3]/1000,vertices[(i*3)+1]/1000,vertices[(i*3)+2]);
+                glVertex3f(vertices[i*3]/1000,vertices[(i*3)+1]/1000,vertices[(i*3)+2] + 10);
                 glColor3f(0.0,1.0,0.0);
             }
         glEnd();
@@ -138,41 +123,40 @@ void car(void) {
 
 void tires(float rotation) {
     GLfloat centerTires[]= {
-        300.0, 150.0, 2.0,
-        600.0, 150.0, 2.0
+        300.0, 150.0, -2.0,
+        600.0, 150.0, -2.0
+        300.0, 150.0, 8.0,
+        600.0, 150.0, 8.0
     };
     int triangleAmount = 20;
     GLfloat radius = 150.0;
     GLfloat twicePI = 2.0f * 3.14f;
-    glPushMatrix();
-        glTranslatef(centerTires[0]/1000,centerTires[1]/1000,0);
-        glRotatef(rotation,0,0,1);
-        glTranslatef(-centerTires[0]/1000,-centerTires[1]/1000,0);
-        glBegin(GL_TRIANGLE_FAN);
-            glVertex3f(centerTires[0]/1000,centerTires[1]/1000,centerTires[2]/1000);
-            glColor3f(0.0,0.0,1.0);
-            for (int i=0; i<=triangleAmount; i++) {
-                glVertex3f((centerTires[0] + (radius * cos(i * twicePI / triangleAmount)))/1000,
-                            (centerTires[1] + (radius * sin(i * twicePI /triangleAmount)))/1000,
-                            centerTires[2]/1000 );
-                glColor3f(1.0,0.0,1.0);
-            }
-        glEnd();
-    glPopMatrix();
-    /*glPushMatrix();
-        glRotatef(rotation,0,0,1);
-        glBegin(GL_TRIANGLE_FAN);
-            glVertex3f(centerTires[0]/1000,centerTires[1]/1000,centerTires[2]/1000);
-            glColor3f(0.0,0.0,1.0);
-            for (int i=0; i<triangleAmount; i++) {
-                glVertex3f((centerTires[0] + (radius * cos(i * twicePI / triangleAmount)))/1000,
-                            (centerTires[1]/1000 + (radius * sin(i * twicePI /triangleAmount)))/1000,
-                            centerTires[2]/1000);
-                glColor3f(1.0,0.0,1.0);
-            }
-        glEnd();
-    glPopMatrix();*/
-
+    int currentTire = 0;
+    int tireCount = 0;
+    while (currentTire < 4) {
+        tireCount = currentTire * 3;
+        glPushMatrix();
+            glTranslatef(centerTires[tireCount]/1000,centerTires[tireCount + 1]/1000,0);
+            glRotatef(rotation,0,0,1);
+            glTranslatef(-centerTires[tireCount]/1000,-centerTires[tireCount + 1]/1000,0);
+            glBegin(GL_TRIANGLE_FAN);
+                glVertex3f(centerTires[tireCount]/1000,centerTires[tireCount + 1]/1000,centerTires[tireCount + 2]);
+                glColor3f(0.0,0.0,1.0);
+                for (int i=0; i<=triangleAmount; i++) {
+                    glVertex3f((centerTires[tireCount] + (radius * cos(i * twicePI / triangleAmount)))/1000,
+                                (centerTires[tireCount + 1] + (radius * sin(i * twicePI /triangleAmount)))/1000,
+                                centerTires[tireCount + 2]);
+                    glVertex3f((centerTires[tireCount] + (radius * cos(i * twicePI / triangleAmount)))/1000,
+                                (centerTires[tireCount + 1] + (radius * sin(i * twicePI /triangleAmount)))/1000,
+                                centerTires[tireCount + 2] + 2);
+                    glColor3f(1.0,0.0,1.0);
+                }
+                glVertex3f(centerTires[tireCount]/1000,centerTires[tireCount + 1]/1000,centerTires[tireCount + 2] + 2);
+            glEnd();
+        glPopMatrix();
+        currentTire = currentTire + 1;
+    }
+/*
     glPushMatrix();
         glTranslatef(centerTires[3]/1000,centerTires[4]/1000,0);
         glRotatef(rotation,0,0,centerTires[5]/1000);
@@ -183,9 +167,12 @@ void tires(float rotation) {
             for (int i=0; i<=triangleAmount; i++) {
                 glVertex3f((centerTires[3] + (radius * cos(i * twicePI / triangleAmount)))/1000,
                             (centerTires[4] + (radius * sin(i * twicePI /triangleAmount)))/1000,
-                            centerTires[5]/1000 );
+                            centerTires[5]);
+                glVertex3f((centerTires[3] + (radius * cos(i * twicePI / triangleAmount)))/1000,
+                            (centerTires[4] + (radius * sin(i * twicePI /triangleAmount)))/1000,
+                            centerTires[5] + 2);
                 glColor3f(1.0,0.0,1.0);
             }
         glEnd();
-    glPopMatrix();
+    glPopMatrix();*/
 }
