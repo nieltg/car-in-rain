9838ee65b308c27ac4612530bab9c778050a3ae8
Daniel <nieltansah@gmail.com>
2018-04-11 06:14:10 +0700

    Test 1

--- src/common/shader_utils.cpp
+++ src/common/shader_utils.cpp
@@ -4,118 +4,124 @@
  * Contributors: Sylvain Beucler
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+#include <SDL2/SDL.h>
 #include <GL/glew.h>
 
 /**
  * Store all the file's contents in memory, useful to pass shaders
- * source code to OpenGL
+ * source code to OpenGL.  Using SDL_RWops for Android asset support.
  */
-char* file_read(const char* filename)
-{
-  FILE* in = fopen(filename, "rb");
-  if (in == NULL) return NULL;
+char* file_read(const char* filename, int* size) {
+	SDL_RWops *rw = SDL_RWFromFile(filename, "rb");
+	if (rw == NULL) return NULL;
 
-  int res_size = BUFSIZ;
-  char* res = (char*)malloc(res_size);
-  int nb_read_total = 0;
+	Sint64 res_size = SDL_RWsize(rw);
+	char* res = (char*)malloc(res_size + 1);
 
-  while (!feof(in) && !ferror(in)) {
-    if (nb_read_total + BUFSIZ > res_size) {
-      if (res_size > 10*1024*1024) break;
-      res_size = res_size * 2;
-      res = (char*)realloc(res, res_size);
-    }
-    char* p_res = res + nb_read_total;
-    nb_read_total += fread(p_res, 1, BUFSIZ, in);
-  }
-  
-  fclose(in);
-  res = (char*)realloc(res, nb_read_total + 1);
-  res[nb_read_total] = '\0';
-  return res;
+	Sint64 nb_read_total = 0, nb_read = 1;
+	char* buf = res;
+	while (nb_read_total < res_size && nb_read != 0) {
+		nb_read = SDL_RWread(rw, buf, 1, (res_size - nb_read_total));
+		nb_read_total += nb_read;
+		buf += nb_read;
+	}
+	SDL_RWclose(rw);
+	if (nb_read_total != res_size) {
+		free(res);
+		return NULL;
+	}
+
+	res[nb_read_total] = '\0';
+	if (size != NULL)
+		*size = nb_read_total;
+	return res;
 }
 
 /**
  * Display compilation errors from the OpenGL shader compiler
  */
-void print_log(GLuint object)
-{
-  GLint log_length = 0;
-  if (glIsShader(object))
-    glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
-  else if (glIsProgram(object))
-    glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
-  else {
-    fprintf(stderr, "printlog: Not a shader or a program\n");
-    return;
-  }
+void print_log(GLuint object) {
+	GLint log_length = 0;
+	if (glIsShader(object)) {
+		glGetShaderiv(object, GL_INFO_LOG_LENGTH, &log_length);
+	} else if (glIsProgram(object)) {
+		glGetProgramiv(object, GL_INFO_LOG_LENGTH, &log_length);
+	} else {
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+					   "printlog: Not a shader or a program");
+		return;
+	}
 
-  char* log = (char*)malloc(log_length);
+	char* log = (char*)malloc(log_length);
 
-  if (glIsShader(object))
-    glGetShaderInfoLog(object, log_length, NULL, log);
-  else if (glIsProgram(object))
-    glGetProgramInfoLog(object, log_length, NULL, log);
+	if (glIsShader(object))
+		glGetShaderInfoLog(object, log_length, NULL, log);
+	else if (glIsProgram(object))
+		glGetProgramInfoLog(object, log_length, NULL, log);
 
-  fprintf(stderr, "%s", log);
-  free(log);
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s\n", log);
+	free(log);
 }
 
 /**
  * Compile the shader from file 'filename', with error handling
  */
-GLuint create_shader(const char* filename, GLenum type)
-{
-  const GLchar* source = file_read(filename);
-  if (source == NULL) {
-    fprintf(stderr, "Error opening %s: ", filename); perror("");
-    return 0;
-  }
-  GLuint res = glCreateShader(type);
-  const GLchar* sources[] = {
-    // Define GLSL version
-#ifdef GL_ES_VERSION_2_0
-    "#version 100\n"  // OpenGL ES 2.0
-#else
-    "#version 120\n"  // OpenGL 2.1
-#endif
-    ,
-    // GLES2 precision specifiers
-#ifdef GL_ES_VERSION_2_0
-    // Define default float precision for fragment shaders:
-    (type == GL_FRAGMENT_SHADER) ?
-    "#ifdef GL_FRAGMENT_PRECISION_HIGH\n"
-    "precision highp float;           \n"
-    "#else                            \n"
-    "precision mediump float;         \n"
-    "#endif                           \n"
-    : ""
-    // Note: OpenGL ES automatically defines this:
-    // #define GL_ES
-#else
-    // Ignore GLES 2 precision specifiers:
-    "#define lowp   \n"
-    "#define mediump\n"
-    "#define highp  \n"
-#endif
-    ,
-    source };
-  glShaderSource(res, 3, sources, NULL);
-  free((void*)source);
+GLuint create_shader(const char* filename, GLenum type) {
+	const GLchar* source = file_read(filename, NULL);
+	if (source == NULL) {
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+					   "Error opening %s: %s", filename, SDL_GetError());
+		return 0;
+	}
+	GLuint res = glCreateShader(type);
 
-  glCompileShader(res);
-  GLint compile_ok = GL_FALSE;
-  glGetShaderiv(res, GL_COMPILE_STATUS, &compile_ok);
-  if (compile_ok == GL_FALSE) {
-    fprintf(stderr, "%s:", filename);
-    print_log(res);
-    glDeleteShader(res);
-    return 0;
-  }
+	// GLSL version
+	const char* version;
+	int profile;
+	SDL_GL_GetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, &profile);
+	if (profile == SDL_GL_CONTEXT_PROFILE_ES)
+		version = "#version 100\n";  // OpenGL ES 2.0
+	else
+		version = "#version 120\n";  // OpenGL 2.1
 
-  return res;
+	// GLES2 precision specifiers
+	const char* precision;
+	precision =
+		"#ifdef GL_ES                        \n"
+		"#  ifdef GL_FRAGMENT_PRECISION_HIGH \n"
+		"     precision highp float;         \n"
+		"#  else                             \n"
+		"     precision mediump float;       \n"
+		"#  endif                            \n"
+		"#else                               \n"
+		// Ignore unsupported precision specifiers
+		"#  define lowp                      \n"
+		"#  define mediump                   \n"
+		"#  define highp                     \n"
+		"#endif                              \n";
+
+	const GLchar* sources[] = {
+		version,
+		precision,
+		source
+	};
+	glShaderSource(res, 3, sources, NULL);
+	free((void*)source);
+
+	glCompileShader(res);
+	GLint compile_ok = GL_FALSE;
+	glGetShaderiv(res, GL_COMPILE_STATUS, &compile_ok);
+	if (compile_ok == GL_FALSE) {
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s:\n", filename);
+		print_log(res);
+		glDeleteShader(res);
+		return 0;
+	}
+
+	return res;
 }
 
 GLuint create_program(const char *vertexfile, const char *fragmentfile) {
@@ -140,7 +146,7 @@ GLuint create_program(const char *vertexfile, const char *fragmentfile) {
 	GLint link_ok = GL_FALSE;
 	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
 	if (!link_ok) {
-		fprintf(stderr, "glLinkProgram:");
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "glLinkProgram:");
 		print_log(program);
 		glDeleteProgram(program);
 		return 0;
@@ -183,7 +189,7 @@ GLuint create_gs_program(const char *vertexfile, const char *geometryfile, const
 	GLint link_ok = GL_FALSE;
 	glGetProgramiv(program, GL_LINK_STATUS, &link_ok);
 	if (!link_ok) {
-		fprintf(stderr, "glLinkProgram:");
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "glLinkProgram:");
 		print_log(program);
 		glDeleteProgram(program);
 		return 0;
@@ -193,7 +199,8 @@ GLuint create_gs_program(const char *vertexfile, const char *geometryfile, const
 }
 #else
 GLuint create_gs_program(const char *vertexfile, const char *geometryfile, const char *fragmentfile, GLint input, GLint output, GLint vertices) {
-	fprintf(stderr, "Missing support for geometry shaders.\n");
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+				   "Missing support for geometry shaders.");
 	return 0;
 }
 #endif
@@ -201,13 +208,32 @@ GLuint create_gs_program(const char *vertexfile, const char *geometryfile, const
 GLint get_attrib(GLuint program, const char *name) {
 	GLint attribute = glGetAttribLocation(program, name);
 	if(attribute == -1)
-		fprintf(stderr, "Could not bind attribute %s\n", name);
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+					   "Could not bind attribute %s", name);
 	return attribute;
 }
 
 GLint get_uniform(GLuint program, const char *name) {
 	GLint uniform = glGetUniformLocation(program, name);
 	if(uniform == -1)
-		fprintf(stderr, "Could not bind uniform %s\n", name);
+		SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR,
+					   "Could not bind uniform %s", name);
 	return uniform;
+}
+
+void print_opengl_info() {
+	int major, minor, profile;
+	SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &major);
+	SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &minor);
+	SDL_GL_GetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, &profile);
+	const char* profile_str = "";
+	if (profile & SDL_GL_CONTEXT_PROFILE_CORE)
+		profile_str = "CORE";
+	if (profile & SDL_GL_CONTEXT_PROFILE_COMPATIBILITY)
+		profile_str = "COMPATIBILITY";
+	if (profile & SDL_GL_CONTEXT_PROFILE_ES)
+		profile_str = "ES";
+
+	SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO,
+				   "OpenGL %d.%d %s", major, minor, profile_str);
 }
--- src/common/shader_utils.h
+++ src/common/shader_utils.h
@@ -6,11 +6,14 @@
 #ifndef _CREATE_SHADER_H
 #define _CREATE_SHADER_H
 #include <GL/glew.h>
-char* file_read(const char* filename);
-void print_log(GLuint object);
-GLuint create_shader(const char* filename, GLenum type);
-GLuint create_program(const char* vertexfile, const char *fragmentfile);
-GLuint create_gs_program(const char* vertexfile, const char *geometryfile, const char *fragmentfile, GLint input, GLint output, GLint vertices);
-GLint get_attrib(GLuint program, const char *name);
-GLint get_uniform(GLuint program, const char *name);
+
+extern char* file_read(const char* filename, int* size);
+extern void print_log(GLuint object);
+extern GLuint create_shader(const char* filename, GLenum type);
+extern GLuint create_program(const char* vertexfile, const char *fragmentfile);
+extern GLuint create_gs_program(const char* vertexfile, const char *geometryfile, const char *fragmentfile, GLint input, GLint output, GLint vertices);
+extern GLint get_attrib(GLuint program, const char *name);
+extern GLint get_uniform(GLuint program, const char *name);
+extern void print_opengl_info();
+
 #endif
