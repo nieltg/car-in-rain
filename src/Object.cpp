9761067073c522a8911769069b5c605dda34e544
Alif Ijlal W <alief.iw@gmail.com>
2018-05-13 12:33:28 +0700

    Restructured Object

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,22 +1,18 @@
 #include "Object.h"
 
-Object::Object(std::string path)
+Object::Object(std::string path) : vBuffer("coord3d"), 
+                                    uvBuffer("texcoord"),
+                                    normBuffer("normals"),
+                                    idxBuffer("elements")
 {
     loaded = loader.LoadFile(path);
     if (!loaded)
     {
         std::cerr << ".obj not loaded" << std::endl;
     }
-}
-
-std::vector<std::vector<glm::vec3>> Object::getVertexPositions()
-{
-    if (!loaded)
-        std::cerr << "Call after .obj is loaded" << std::endl;
     else
     {
         //Load all meshes here
-        std::vector<std::vector<glm::vec3>> VertexPositions;
         for (int i = 0; i < loader.LoadedMeshes.size(); i++)
         {
             //Load vertices per mesh
@@ -29,26 +25,9 @@ std::vector<std::vector<glm::vec3>> Object::getVertexPositions()
                     currentMesh.Vertices[j].Position.Z);
                 currentPositions.push_back(currentVertex);
             }
-            VertexPositions.push_back(currentPositions);    
-        }
-        return VertexPositions; 
-    }
-}
-
-
-std::vector<std::vector<glm::vec3>> Object::getVertexNormals()
-{
-    if (!loaded)
-        std::cerr << "Call  after .obj is loaded" << std::endl;
-    else
-    {
-        //Load all meshes here
-        std::vector<std::vector<glm::vec3>> VertexNormals;
-        for (int i = 0; i < loader.LoadedMeshes.size(); i++)
-        {
-            //Load vertices per mesh
+            vertexPositions.push_back(currentPositions);
+            //load normals per mesh
             std::vector<glm::vec3> currentNormals;
-            objl::Mesh currentMesh = loader.LoadedMeshes[i];
             for (int j = 0; j < currentMesh.Vertices.size(); j++)
             {
                 glm::vec3 currentVertex(currentMesh.Vertices[j].Normal.X,
@@ -56,50 +35,18 @@ std::vector<std::vector<glm::vec3>> Object::getVertexNormals()
                     currentMesh.Vertices[j].Normal.Z);
                 currentNormals.push_back(currentVertex);
             }
-            VertexNormals.push_back(currentNormals);    
-        }
-        return VertexNormals; 
-    }
-}
-
-std::vector<std::vector<glm::vec2>> Object::getVertexTexCoors()
-{
-    if (!loaded)
-        std::cerr << "Call  after .obj is loaded" << std::endl;
-    else
-    {
-        //Load all meshes here
-        std::vector<std::vector<glm::vec2>> VertexTexCoors;
-        for (int i = 0; i < loader.LoadedMeshes.size(); i++)
-        {
-            //Load vertices per mesh
+            vertexNormals.push_back(currentNormals);
+            //Load uvs per mesh
             std::vector<glm::vec2> currentTexCoors;
-            objl::Mesh currentMesh = loader.LoadedMeshes[i];
             for (int j = 0; j < currentMesh.Vertices.size(); j++)
             {
                 glm::vec2 currentVertex(currentMesh.Vertices[j].TextureCoordinate.X,
                     currentMesh.Vertices[j].TextureCoordinate.Y);
                 currentTexCoors.push_back(currentVertex);
             }
-            VertexTexCoors.push_back(currentTexCoors);    
-        }
-        return VertexTexCoors; 
-    }
-}
-
-std::vector<std::vector<glm::vec3>> Object::getIndices()
-{
-    if (!loaded)
-        std::cerr << "Call  after .obj is loaded" << std::endl;
-    else
-    {
-        //Load all meshes here
-        std::vector<std::vector<glm::vec3>> Indices;
-        for (int i = 0; i < loader.LoadedMeshes.size(); i++)
-        {
-            //Load vertices per mesh
+            vertexTexCoors.push_back(currentTexCoors);  
+            //Load indices per mesh
             std::vector<glm::vec3> currentIndices;
-            objl::Mesh currentMesh = loader.LoadedMeshes[i];
             for (int j = 0; j < currentMesh.Vertices.size(); j+=3)
             {
                 glm::vec3 currentIndice(currentMesh.Indices[j],
@@ -107,8 +54,9 @@ std::vector<std::vector<glm::vec3>> Object::getIndices()
                     currentMesh.Indices[j+2]);
                 currentIndices.push_back(currentIndice);
             }
-            Indices.push_back(currentIndices);    
+            indices.push_back(currentIndices);   
         }
-        return Indices; 
+        //Do things with Buffers
+
     }
 }
