6db0d5aa6884587d3a45d61d21ac9d1e337d6650
Alif Ijlal W <alief.iw@gmail.com>
2018-05-15 20:40:10 +0700

    Added multisampling & shininess; still no luck on lighting

--- mesh.f.glsl
+++ mesh.f.glsl
@@ -38,7 +38,7 @@ void main(void) {
     vec4(0.2, 0.2, 0.2, 1.0),
     vec4(1.0, 0.8, 0.8, 1.0),
     vec4(1.0, 1.0, 1.0, 1.0),
-    5.0
+    256.0
   );
 
   vec3 normalDirection = normalize(f_norm);
