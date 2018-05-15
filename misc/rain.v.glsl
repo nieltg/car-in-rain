25ead2267593dcf4552f6737ac6a03513cd7ef62
Daniel <nieltansah@gmail.com>
2018-05-15 20:59:41 +0700

    Add shader for rain
    app: Rain: Fix shader paths

---
+++ misc/rain.v.glsl
@@ -0,0 +1,19 @@
+#version 140
+
+attribute vec3 coord3d;
+
+uniform mat4 mvp;
+uniform uint time;
+
+void main (void) {
+  vec3 pos = coord3d;
+  pos.y = mod(pos.y - (time / 500.0), 4);
+
+  if ((gl_VertexID & 1) == 0) {
+    pos.y += 0.1;
+  }
+
+  pos.y -= 1.5;
+
+  gl_Position = mvp * vec4(pos, 1.0);
+}
