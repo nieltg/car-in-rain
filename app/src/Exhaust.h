d78e8b6d0b7050e29c5a7cf970edbef2bb789045
Daniel <nieltansah@gmail.com>
2018-05-16 11:31:23 +0700

    Add exhaust things

---
+++ app/src/Exhaust.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+
+#include <glbinding/gl/gl.h>
+
+#include <globjects/base/File.h>
+#include <globjects/base/StringTemplate.h>
+#include <globjects/Buffer.h>
+#include <globjects/Program.h>
+#include <globjects/Shader.h>
+#include <globjects/Texture.h>
+#include <globjects/VertexArray.h>
+
+
+class Exhaust {
+public:
+  const int N_PARTICLE = 1000;
+
+  Exhaust (void);
+
+  void draw (glm::mat4 _v, glm::mat4 _p);
+
+private:
+  std::unique_ptr<globjects::Program> program;
+  std::unique_ptr<globjects::File> shader_v_src;
+  std::unique_ptr<globjects::AbstractStringSource> shader_v_preprocessed;
+  std::unique_ptr<globjects::Shader> shader_v;
+  std::unique_ptr<globjects::File> shader_f_src;
+  std::unique_ptr<globjects::AbstractStringSource> shader_f_preprocessed;
+  std::unique_ptr<globjects::Shader> shader_f;
+
+  gl::GLint i_attr_coord3d;
+  gl::GLint i_uniform_mvp;
+  gl::GLint i_uniform_time;
+
+  std::unique_ptr<globjects::VertexArray> vao;
+  std::unique_ptr<globjects::Buffer> b_vertices;
+
+  size_t draw_len;
+};
---
+++ misc/exhaust.v.glsl
@@ -0,0 +1,40 @@
+#version 140
+
+attribute vec3 coord3d;
+
+uniform mat4 mvp;
+uniform uint time;
+varying vec2 uv;
+
+void main (void) {
+  float time2 = time;
+  float time1 = time2 / 10000.0;
+
+  float frame = mod(coord3d.z - 0.5 + time1, 1.6);
+
+  vec3 pos;
+  pos.x = -coord3d.x / sqrt(frame);
+  pos.y = coord3d.x * frame;
+  pos.z = coord3d.y * frame;
+
+  int id = gl_VertexID & 3;
+  switch (id) {
+    case 0:
+      uv = vec2(0.0, 0.0);
+      break;
+    case 1:
+      pos += vec3(0.5, 0.0, 0.0);
+      uv = vec2(1.0, 0.0);
+      break;
+    case 2:
+      pos += vec3(0.5, 0.5, 0.0);
+      uv = vec2(1.0, 1.0);
+      break;
+    case 3:
+      pos += vec3(0.0, 0.5, 0.0);
+      uv = vec2(0.0, 1.0);
+      break;
+  }
+
+  gl_Position = mvp * vec4(pos, 1.0);
+}
