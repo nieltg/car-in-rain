207ecd9c3f1ae0e06957eca954b2715a71d691ea
Daniel <nieltansah@gmail.com>
2018-05-29 18:13:55 +0700

    Fix exhaust particle billboard

--- exhaust.f.glsl
+++ exhaust.f.glsl
@@ -1,9 +1,14 @@
 #version 140
 
 varying vec2 f_texcoord;
+varying float f_alpha;
+
 uniform sampler2D texture;
 
 
 void main (void) {
-  gl_FragColor = texture2D(texture, f_texcoord);
+  vec4 col = texture2D(texture, f_texcoord);
+  col.a *= f_alpha;
+
+  gl_FragColor = col;
 }
--- exhaust.v.glsl
+++ exhaust.v.glsl
@@ -8,38 +8,49 @@ uniform mat4 p;
 uniform uint time;
 
 varying vec2 f_texcoord;
+varying float f_alpha;
 
 
 void main (void) {
   float time2 = time;
-  float time1 = time2 / 10000.0;
+  float time1 = time2 / 5000.0;
 
   float frame = 0.1 + mod(coord3d.z - 0.5 + time1, 1.5);
 
-  vec3 pos;
-  pos.x = -coord3d.x / sqrt(frame);
-  pos.y = coord3d.x * frame;
-  pos.z = coord3d.y * frame;
+  vec3 center;
+  center.z = -5.5 + coord3d.x / sqrt(frame);
+  center.y = coord3d.x * frame;
+  center.x = coord3d.y * frame;
 
-  int id = gl_VertexID & 3;
+  f_alpha = 1.5 - frame;
+
+  vec3 cam_r = vec3(v[0][0], v[1][0], v[2][0]);
+  vec3 cam_u = vec3(v[0][1], v[1][1], v[2][1]);
+
+  float size = 0.5 * frame;
+
+  vec3 pos;
+
+  int id = gl_VertexID & 0x3;
   switch (id) {
     case 0:
+      pos = center - (cam_r + cam_u) * size;
       f_texcoord = vec2(0.0, 0.0);
       break;
     case 1:
-      pos += vec3(0.3, 0.0, 0.0);
+      pos = center - (cam_r - cam_u) * size;
       f_texcoord = vec2(1.0, 0.0);
       break;
     case 2:
-      pos += vec3(0.3, 0.3, 0.0);
+      pos = center + (cam_r + cam_u) * size;
       f_texcoord = vec2(1.0, 1.0);
       break;
     case 3:
-      pos += vec3(0.0, 0.3, 0.0);
+      pos = center + (cam_r - cam_u) * size;
       f_texcoord = vec2(0.0, 1.0);
       break;
   }
 
-  mat4 mvp = p * v * m;
-  gl_Position = mvp * vec4(pos, 1.0);
+  mat4 vp = p * v;
+  gl_Position = vp * vec4(pos, 1.0);
 }
